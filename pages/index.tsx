62df0dfdcb527dd8608821bf955e290e952827f2
TanishqSingla <tanishqsingla00@gmail.com>
2022-12-08 01:00:23 +0530

    Style main.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,25 @@
 import Head from "next/head";
+import { GrGithub, GrTwitter, GrLinkedin } from "react-icons/gr";
+import { IoMdDownload } from "react-icons/io";
 
 export default function Home() {
+	const socials = [
+		{
+			name: "Github",
+			link: "https://www.github.com/TanishqSingla",
+			logo: <GrGithub size="24" color="#344e41" />,
+		},
+		{
+			name: "Twitter",
+			link: "https://www.twitter.com/TanishqSingla_",
+			logo: <GrTwitter size="24" color="#344e41" />,
+		},
+		{
+			name: "LinkedIn",
+			link: "https://www.linkedin.com/in/tanishqsingla",
+			logo: <GrLinkedin size="24" color="#344e41" />,
+		},
+	];
 	return (
 		<>
 			<Head>
@@ -18,12 +37,27 @@ export default function Home() {
 			</div>
 			<div className="about-me py-2">
 				<h3>About me</h3>
-				<p className="indent-4">I am a fullstack developer and I like to love to make webapps</p>
+				<p className="indent-4">
+					I am a fullstack developer and I like to love to make webapps
+				</p>
 			</div>
 			<div className="social">
 				<h3>Find me on</h3>
 				<div className="social-links">
-					<a href="https://www.github.com/TanishqSingla">
+					{socials.map((social) => (
+						<a href={social.link} key={social.name}>
+							{social.logo}
+							{social.name}
+						</a>
+					))}
+				</div>
+			</div>
+			<div>
+				<h3>Dowload resume</h3>
+				<div className="social-links">
+					<a href="" download className="px-4">
+						<IoMdDownload color="#344e41" size="24" />
+						Resume
 					</a>
 				</div>
 			</div>
