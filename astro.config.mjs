16e7284158eae7ca639381c9d8f17a3360715f8f
TanishqSingla <tanishqsingla00@gmail.com>
2025-09-30 21:42:15 +0530

    feat: use github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,6 @@ import expressiveCode from 'astro-expressive-code'
 import { expressiveCodeOptions } from './src/site.config'
 import icon from 'astro-icon'
 
-import vercel from '@astrojs/vercel/serverless'
-
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
@@ -44,8 +42,5 @@ export default defineConfig({
 		}
 	},
 	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	})
+	output: 'static'
 })
